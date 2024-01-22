6acf05367534b6369980b6e356321227bc06eb49
niccbytes <140906373+niccbytes@users.noreply.github.com>
2024-01-22 13:00:31 -0600

    able to render project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,30 +1,7 @@
-import React from 'react'; // Make sure to import React
+
 import './index.scss';
 
-const projects = [
-  {
-    title: 'Project 1',
-    description: 'Description for Project 1.',
-  },
-  {
-    title: 'Project 2',
-    description: 'Description for Project 2.',
-  },
-  {
-    title: 'Project 3',
-    description: 'Description for Project 3.',
-  },
-];
+const projects = () => 'hello'
 
-const Projects = () => (
-  <div className="app">
-    <h1>My Projects</h1>
-    <div className="projects-container">
-      {projects.map((project, index) => (
-        <Projects key={index} {...project} />
-      ))}
-    </div>
-  </div>
-);
 
-export default Projects;
+export default projects;
