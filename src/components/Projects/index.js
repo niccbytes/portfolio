d2bfd5525e2b9b010962beea461f7ed553133ee5
niccbytes <140906373+niccbytes@users.noreply.github.com>
2024-01-22 16:23:35 -0600

    added projects and got images working

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,18 +1,27 @@
-
+import React from 'react';
 import './index.scss';
+import GrocerEZImage from '../../assets/images/grocerEZ.png';
+import BictorfyImage from '../../assets/images/bictorfylogo.JPG';
+import SnakeGameImage from '../../assets/images/snakegame.png';
 
 const projectsData = [
   {
-    title: 'Project 1',
-    description: 'Description for Project 1.',
+    title: 'grocerEZ',
+    description: 'a grocery application that makes going to the grocery store a breeze! with grocerEZ your indecisiveness disappears and worries about the trip there!',
+    image: GrocerEZImage,
+    link: 'https://kristin611.github.io/GrocerEZ/',
   },
   {
-    title: 'Project 2',
-    description: 'Description for Project 2.',
+    title: 'Bictorfy',
+    description: 'simple spotify clone',
+    image: BictorfyImage,
+    link: 'https://github.com/gracecatk/bictorfy?tab=readme-ov-file',
   },
   {
-    title: 'Project 3',
-    description: 'Description for Project 3.',
+    title: 'snake game',
+    description: 'simple snake game',
+    image: SnakeGameImage,
+    link: 'https://niccbytes.github.io/snakegame/',
   },
 ];
 
@@ -22,14 +31,16 @@ const projects = () => {
       <h1>My Projects</h1>
       {projectsData.map((project, index) => (
         <div key={index} className="project-item">
+          <img src={project.image} alt={project.title} />
           <h2>{project.title}</h2>
           <p>{project.description}</p>
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            View Project
+          </a>
         </div>
       ))}
     </div>
   );
 };
-
-
 
 export default projects;
