73ad82f04ce3ebfb52cf68f364386636528af4c0
niccbytes <140906373+niccbytes@users.noreply.github.com>
2024-01-22 12:35:16 -0600

    got contact form working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -42,11 +42,7 @@ const Contact = () => {
               idx={15}
             />
           </h1>
-          <p>
-            I am interested in freelance opportunities - especially on ambitious
-            or large projects. However, if you have any other requests or
-            questions, don't hesitate to contact me using below form either.
-          </p>
+       
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
               <ul>
@@ -84,22 +80,24 @@ const Contact = () => {
           </div>
         </div>
         <div className="info-map">
-          Slobodan Gajić,
+          Nick Amaro,
           <br />
-          Serbia,
+          Houston,
           <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
+          August 2nd, 2000 <br />
+           <br />
+           for inquiries
           <br />
-          <span>freelancerslobodan@gmail.com</span>
+          <span>nicka3mail@gmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
-            </Marker>
-          </MapContainer>
+        <MapContainer center={[29.7604, -95.3698]} zoom={12}>
+  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+  <Marker position={[29.7604, -95.3698]}>
+    <Popup>Central Houston - Come explore!</Popup>
+  </Marker>
+</MapContainer>
+
         </div>
       </div>
    
--- src/components/Layout/sidebar/index.js
+++ src/components/Layout/sidebar/index.js
@@ -19,7 +19,7 @@ const Sidebar = () => (
       <NavLink exact={true} activeClassName="active" className="about-link" to="/about">
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact={true} activeClassName="active" className="contact-link" to="/conta">
+      <NavLink exact={true} activeClassName="active" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
