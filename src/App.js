542e528e4589fe733005ce575409ce0950ab225c
niccbytes <140906373+niccbytes@users.noreply.github.com>
2024-01-21 19:43:08 -0600

    added a not working contact page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from '../src/components/Home'
+import Home from '../src/components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
-
-function App(){
+function App() {
   return (
-
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      </Route>
-    </Routes>
-
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
-
-  )
+  );
 }
-
 
 export default App;
