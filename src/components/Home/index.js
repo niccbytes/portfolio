4c5407a5af8f6b417a3b5ddbed5d89ea6484e694
niccbytes <140906373+niccbytes@users.noreply.github.com>
2024-01-22 19:09:37 -0600

    added my resume

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
-import React from 'react'; // Make sure to import React
-import { Link } from 'react-router-dom'; // Import Link component
+import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import ResumeIcon from '../../assets/images/resume1.png'; // Add the correct path to your resume image
 import './index.scss';
 
 const Home = () => {
@@ -12,10 +13,21 @@ const Home = () => {
           <img src={LogoTitle} alt="developer" />
           Nicc
           <br />
-          web Developer
+          Web Developer
         </h1>
         <h2>Frontend Developer | JavaScript Scripter | Entrepreneur </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <div className="resume-section">
+        <a
+          href="../../assets/images/resume1.png" // Replace with the correct path or URL for your resume
+          download="Nicc_Resume"
+        >
+          <img src={ResumeIcon} alt="resume" />
+          <button>Download Resume</button>
+        </a>
       </div>
     </div>
   );
